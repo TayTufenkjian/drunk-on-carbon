d7e0735d9b79dfe49ffea0cf8b61d009d9ed1c2b
TayTufenkjian <taytuf@gmail.com>
2022-03-17 10:02:06 -0400

    created Python project and apps

---
+++ drunk_on_carbon/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for drunk_on_carbon project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drunk_on_carbon.settings')
+
+application = get_asgi_application()
---
+++ drunk_on_carbon/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for drunk_on_carbon project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drunk_on_carbon.settings')
+
+application = get_wsgi_application()
