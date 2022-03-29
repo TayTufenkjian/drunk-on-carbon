b9084ab93bbfdbd7ba2413277049084ed09e2405
TayTufenkjian <taytuf@gmail.com>
2022-03-29 15:34:37 -0400

    added login test case

--- carbon/tests/test_views.py
+++ carbon/tests/test_views.py
@@ -7,13 +7,13 @@ from ..models import User, SavedEstimate
 class CreateAccountTestCase(TestCase):
 
     def test_create_account_get(self):
-        """Sending a GET request loads the create_account.html page"""
+        """Sending a GET request loads the create_account template"""
         c = Client()
         response = c.get(reverse("create_account"))
         self.assertTemplateUsed(response, "create_account.html")
 
     def test_create_account(self):
-        """Can create a new user account when password and confirmation match"""
+        """Can create a new user account via POST when password and confirmation match"""
         user_data = {
             "username": "kangaroo123",
             "email": "kanga@roo.notreal",
@@ -35,3 +35,31 @@ class CreateAccountTestCase(TestCase):
         c = Client()
         response = c.post(reverse("create_account"), data = user_data)
         assert response.context["message"] == "Passwords must match. Please check your password and confirmation password, then try again."
+
+
+class LoginTestCase(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        user = User.objects.create_user(username="helloworld", email="hello@world.net", password="pass123word456")
+        user.save()
+
+    def test_login_success(self):
+        """User can log in when username and password match"""
+        login_data = {
+            "username": "helloworld", 
+            "password": "pass123word456"
+        }
+        c = Client()
+        response = c.post(reverse("login"), data = login_data)
+        self.assertRedirects(response, expected_url="/")
+    
+    def test_login_failure(self):
+        """User cannot log in when username and password do not match"""
+        login_data = {
+            "username": "helloworld", 
+            "password": "badpassword"
+        }
+        c = Client()
+        response = c.post(reverse("login"), data = login_data)
+        assert response.context["message"] == "Invalid username and/or password."
