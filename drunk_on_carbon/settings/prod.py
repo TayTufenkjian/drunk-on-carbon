7ee04cb063fbf9316d1ec98fa1c7e6f18c3590dd
TayTufenkjian <taytuf@gmail.com>
2022-04-08 13:05:05 -0400

    added logging to prod settings

--- drunk_on_carbon/settings/prod.py
+++ drunk_on_carbon/settings/prod.py
@@ -10,3 +10,20 @@ ALLOWED_HOSTS = ['.herokuapp.com']
 # For these settings to work, the web server must redirect all HTTP traffic to HTTPS, and only transmit HTTPS requests to Django
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
+
+# Logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
