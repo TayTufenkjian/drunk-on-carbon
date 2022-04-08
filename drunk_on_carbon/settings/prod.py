d2aea9e2657d573c18b7bc0125b4542b98e7a943
TayTufenkjian <taytuf@gmail.com>
2022-04-08 10:38:03 -0400

    added whitenoise for serving static files in prod

--- drunk_on_carbon/settings/prod.py
+++ drunk_on_carbon/settings/prod.py
@@ -5,3 +5,21 @@ SECRET_KEY = str(os.getenv('PROD_SECRET_KEY'))
 DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com']
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+MIDDLEWARE_CLASSES = (
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware'
+)
