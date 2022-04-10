5c172911033d6c70718416dd556b32d7de8b3865
TayTufenkjian <taytuf@gmail.com>
2022-04-10 13:02:00 -0400

    redirect non-HTTPS requests to HTTPS in prod

--- drunk_on_carbon/settings/prod.py
+++ drunk_on_carbon/settings/prod.py
@@ -9,6 +9,7 @@ ALLOWED_HOSTS = ['.herokuapp.com']
 
 # Settings to avoid transmitting sensitive cookies over HTTP accidentally
 # For these settings to work, the web server must redirect all HTTP traffic to HTTPS, and only transmit HTTPS requests to Django
+SECURE_SSL_REDIRECT = True
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
